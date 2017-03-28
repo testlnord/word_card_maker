a86aa28dc1435087784490f23c6b0c916dae7ca4
Lara Kolesnichenko <larkkinn@gmail.com>
2017-03-28 17:56:07 +0300

    add Fake Preferences menu and get the context

--- firefox_plugin/DoubleClickDec.js
+++ firefox_plugin/DoubleClickDec.js
@@ -1,6 +1,22 @@
-document.body.style.border = "5px solid blue";
+//document.body.style.border = "5px solid blue";
 var html = "";
+var surroundings = "";
 var currentDiv = null;
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+function onGot(item) {
+  var color = "blue";
+  if (item.color) {
+    color = item.color;
+  }
+  document.body.style.border = "10px solid " + color;
+}
+
+var getting = browser.storage.local.get("color");
+getting.then(onGot, onError);
 
 var myPort = browser.runtime.connect({name:"port-from-dblclick"});
 myPort.postMessage({greeting: "hello from content script"});
@@ -9,7 +25,7 @@ myPort.onMessage.addListener(function(m) {
   console.log("In content script, received message from background script: ");
   console.log(m.message);
   if (m.message === "lumos!") {
-    currentDiv.style.background = "blue";
+      currentDiv.style.background = "blue";
   }
 });
 
@@ -24,41 +40,44 @@ function handleDoubleClick() {
                     .cloneContents());
             }
             html = container.innerHTML;
+            surroundings = sel.getRangeAt(0).startContainer.parentNode.innerHTML;
         }
     } else if (typeof document.selection != "undefined") {
         if (document.selection.type == "Text") {
             html = document.selection.createRange()
                 .htmlText;
+            //surroundings = document.selection.createRange().parentElement().innerHTML;
+
         }
     }
 
     var divf = document.createElement("div");
     divf.style.width = "100px";
-    divf.style.height = "100px";
+    divf.style.height = "fit-content";
     divf.style.background = "red";
     divf.style.color = "white";
     divf.style.visibility = "visible";
-    divf.innerHTML = html;
-    divf.style.position = 'absolute'
+    divf.innerHTML = html + "\nTranslation" + surroundings;
+    divf.style.position = 'absolute';
 
-    var scroll = document.documentElement.scrollTop || document.body.scrollTop
+    var scroll = document.documentElement.scrollTop || document.body.scrollTop;
 
-    divf.style.top = scroll + 10 + 'px'
+    divf.style.top = scroll + 10 + 'px';
 
-    myPort.postMessage({message: "new card!"})
+    myPort.postMessage({message: "new card!"});
     if (document.body.contains(currentDiv)) {
-      document.body.replaceChild(divf, currentDiv);
-      currentDiv = divf;
+        document.body.replaceChild(divf, currentDiv);
+        currentDiv = divf;
     } else {
       currentDiv = document.body.appendChild(divf);
     }
 
-    console.log(html);
+    //console.log(html);
+    var text = String(html);
+    console.log(text);
+    //console.log(html.stripTags());
 };
 
-function handleKeyDown () {
-  document.getElementById("divf").style.visibility = "visible";
-}
 
 window.addEventListener("dblclick",
     function() {
