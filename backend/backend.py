a224b9a93224c92dfc1b1be9b3bb7aa33b5bcd57
nosov-pvl <nosov-pvl@yandex.ru>
2017-03-31 15:12:54 +0300

    Add backend user authentification

--- backend/backend.py
+++ backend/backend.py
@@ -1,9 +1,12 @@
 import requests
 from typing import Dict
 from flask import Flask
-from flask_restful import reqparse, Resource, Api
+from flask_restful import reqparse, Resource, Api, abort
+from flask_jwt import JWT, jwt_required, current_identity
 import settings
 from database import database_methods
+import user_methods
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,8 +15,25 @@ parser = reqparse.RequestParser()
 parser.add_argument('word', type=str, required=True, help='Word for translation cannot be blank')
 parser.add_argument('translation', type=str)
 parser.add_argument('context', type=str)
+parser.add_argument('deck', type=str)
 
 methods = database_methods.DatabaseMethods(settings.DB_USER, settings.DB_PASSWORD, "localhost", settings.DB_NAME)
+
+
+def authenticate(username, password):
+    user: user_methods.User = methods.retrieve_user(username, password)
+    if user:
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    user: user_methods.User = methods.retrieve_user_by_id(user_id)
+    if user:
+        return user
+
+app.config['SECRET_KEY'] = settings.SECRET_KEY
+jwt = JWT(app, authenticate, identity)
 
 
 def yandex_translate(word: str) -> str:
@@ -30,12 +50,23 @@ class Translator(Resource):
 
 
 class Cards(Resource):
+    decorators = [jwt_required()]
+
     def post(self):
         word: str = parser.parse_args()['word']
         translation: str = parser.parse_args()['translation']
         context: str = parser.parse_args()['context']
-        methods.insert_card(word, translation, context, 'Public deck')
+        deck: str = parser.parse_args().get('deck', None)
+        if deck is None:
+            methods.insert_card(word, translation, context, " ".join([current_identity.username, "deck"]))
+        else:
+            has_rights = methods.check_if_user_has_deck(current_identity.id, deck)
+            if not has_rights:
+                abort(400)
+            methods.insert_card(word, translation, context, deck)
+
         return {'word': word, 'translation': translation, 'context': context}
+
 
 api.add_resource(Translator, '/dict/translation')
 api.add_resource(Cards, '/cards/add')
--- backend/database/database_methods.py
+++ backend/database/database_methods.py
@@ -1,5 +1,7 @@
 import psycopg2 as pg_driver
-
+from werkzeug.security import safe_str_cmp
+import hashlib
+import user_methods
 
 class DatabaseMethods:
 
@@ -15,3 +17,89 @@ class DatabaseMethods:
             cur.execute(command)
         finally:
             self.db.commit()
+            cur.close()
+
+    def add_deck(self, username: str, deck_name=None):
+
+        if deck_name is None:
+            deck_name = " ".join([username, "deck"])
+        command: str = "INSERT INTO Deck(name, user_id) VALUES ('{}', (SELECT id FROM USERS WHERE login='{}'));".\
+            format(deck_name, username)
+        cur = self.db.cursor()
+        try:
+            cur.execute(command)
+        finally:
+            self.db.commit()
+            cur.close()
+
+    def add_user(self, username: str, password: str):
+        command: str = "INSERT INTO Users(login, password_hash) VALUES ('{}', '{}');".\
+            format(username, hashlib.sha512(password.encode('utf-8')).hexdigest())
+
+        cur = self.db.cursor()
+        try:
+            cur.execute(command)
+        finally:
+            self.db.commit()
+            cur.close()
+        self.add_deck(username)
+
+    def __get_user(self, username) -> (str, str, str):
+
+        command: str = "SELECT * FROM Users WHERE login='{}'".format(username)
+        user_data = (None, None, None)
+        cur = self.db.cursor()
+
+        try:
+            cur.execute(command)
+            user_data = cur.fetchone()
+        finally:
+            self.db.commit()
+            cur.close()
+        return user_data
+
+    def retrieve_user(self, username: str, password: str) -> user_methods.User:
+
+        user_data = self.__get_user(username)
+
+        if user_data is None:
+            self.add_user(username, password)
+            user_data = self.__get_user(username)
+
+        if user_data and user_data[2] is None or safe_str_cmp(user_data[2],
+                                                              hashlib.sha512(password.encode('utf-8')).hexdigest()):
+            return user_methods.User(user_data[0], username, password)
+
+        return None
+
+    def retrieve_user_by_id(self, user_id) -> user_methods.User:
+        command: str = "SELECT * FROM Users WHERE id='{}'".format(user_id)
+
+        user_data = (None, None, None)
+        cur = self.db.cursor()
+
+        try:
+            cur.execute(command)
+            user_data = cur.fetchone()
+        finally:
+            self.db.commit()
+            cur.close()
+
+        if user_data:
+            return user_methods.User(*user_data)
+
+        return None
+
+    def check_if_user_has_deck(self, user_id: str, deck_name: str) -> bool:
+        has_rights = False
+        command: str = "SELECT * FROM Deck WHERE name='{}' and user_id='{}'".format(deck_name, user_id)
+        cur = self.db.cursor()
+
+        try:
+            cur.execute(command)
+            if cur.fetchone():
+                has_rights = True
+        finally:
+            self.db.commit()
+            cur.close()
+        return has_rights
